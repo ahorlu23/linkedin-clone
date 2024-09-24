36886790d6b1bb202159b550c895c26b28230904
ahorlu23 <nobleahorlu4@gmail.com>
2024-09-24 00:34:20 +0000

    Added some functionalities

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,35 @@
 import styled from "styled-components";
+import { useState } from "react";
+import PostModal from "./PostModal";
 
 const Main = (props) => {
+  const [showModal, setShowModal] = useState("close");
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (e.target !== e.currentTarget) {
+      return;
+    }
+
+    switch (showModal) {
+      case "open":
+        setShowModal("close");
+        break;
+      case "close":
+        setShowModal("open");
+        break;
+      default:
+        setShowModal("close");
+        break;
+    }
+  };
+
   return (
     <Container>
-      <ShareBox>Share
+      <ShareBox>
         <div>
           <img src="/images/user.svg" alt="" />
-          <button>Start a post</button>
+          <button onClick={handleClick}>Start a post</button>
         </div>
 
         <div>
@@ -55,12 +78,12 @@ const Main = (props) => {
           <SocialCounts>
             <li>
               <button>
-                <img 
-                  src="https://static-exp1.licdn.com/sc/h/d310t2g24pvdy4pt1jkedo4yb" 
+                <img
+                  src="https://static-exp1.licdn.com/sc/h/d310t2g24pvdy4pt1jkedo4yb"
                   alt=""
                 />
-                <img 
-                  src="https://static-exp1.licdn.com/sc/h/5thsbmikm6a8uov24ygwd914f" 
+                <img
+                  src="https://static-exp1.licdn.com/sc/h/5thsbmikm6a8uov24ygwd914f"
                   alt=""
                 />
                 <span>75</span>
@@ -88,9 +111,9 @@ const Main = (props) => {
               <span>Send</span>
             </button>
           </SocialActions>
-          
         </Article>
       </div>
+      <PostModal showModal={showModal} handleClick={handleClick} />
     </Container>
   );
 };
@@ -111,7 +134,7 @@ const CommonCard = styled.div`
 `;
 
 const ShareBox = styled(CommonCard)`
-  dispaly: flex;
+  display: flex;
   flex-direction: column;
   color: #958b7b;
   margin: 0 0 8px;
@@ -124,7 +147,7 @@ const ShareBox = styled(CommonCard)`
       line-height: 1.5;
       min-height: 48px;
       background: transparent;
-      border:none;
+      border: none;
       display: flex;
       align-items: center;
       font-weight: 600;
@@ -132,7 +155,7 @@ const ShareBox = styled(CommonCard)`
     &:first-child {
       display: flex;
       align-items: center;
-      padding: 8px 16px 0px 16px;
+      padding: 8px 16px 0 16px;
       img {
         width: 48px;
         border-radius: 50%;
@@ -148,18 +171,17 @@ const ShareBox = styled(CommonCard)`
         text-align: left;
       }
     }
-    &: nth-child(2) {
+    &:nth-child(2) {
       display: flex;
       flex-wrap: wrap;
       justify-content: space-around;
       padding-bottom: 4px;
-
       button {
         img {
           margin: 0 4px 0 -2px;
         }
         span {
-          color: #70b5f9
+          color: #70b5f9;
         }
       }
     }
@@ -180,38 +202,36 @@ const SharedActor = styled.div`
   align-items: center;
   display: flex;
   a {
-      margin-right: 12px;
-      flex-grow: 1;
-      overflow: hidden;
+    margin-right: 12px;
+    flex-grow: 1;
+    overflow: hidden;
+    display: flex;
+    text-decoration: none;
+    img {
+      width: 48px;
+      height: 48px;
+    }
+    & > div {
       display: flex;
-      text-decoration: none;
-
-      img {
-        width: 48px;
-        height: 48px;
-      }
-      
-      & > div {
-        display: flex;
-        flex-direction: column;
-        flex-grow: 1;
-        flex-basis: 0;
-        margin-left: 8px;
-        overflow: hidden;
-        span {
-          text-align: left;
-          &:first-child {
-            font-size: 14px;
-            font-weight: 700;
-            color: rgba(0, 0, 0, 1);
-          }
-          &:nth-child(n+1) {
-            font-size: 12px;
-            color: rgba(0, 0, 0, 0.6);
-          }
+      flex-direction: column;
+      flex-grow: 1;
+      flex-basis: 0;
+      margin-left: 8px;
+      overflow: hidden;
+      span {
+        text-align: left;
+        &:first-child {
+          font-size: 14px;
+          font-weight: 700;
+          color: rgba(0, 0, 0, 1);
+        }
+        &:nth-child(n + 1) {
+          font-size: 12px;
+          color: rgba(0, 0, 0, 0.6);
         }
       }
     }
+  }
   button {
     position: absolute;
     right: 12px;
@@ -228,15 +248,13 @@ const Description = styled.div`
   color: rgba(0, 0, 0, 0.9);
   font-size: 14px;
   text-align: left;
-
 `;
-
 
 const SharedImg = styled.div`
   margin-top: 8px;
   width: 100%;
-  display: block:
-  position: reative;
+  display: block;
+  position: relative;
   background-color: #f9fafb;
   img {
     object-fit: contain;
@@ -250,39 +268,26 @@ const SocialCounts = styled.ul`
   display: flex;
   align-items: flex-start;
   overflow: auto;
-  margin: 0 16px;
-  padding: 8px 0;
-  border-bottom: 1px solid #e9e5df;
-  list-style: none;
-  li {
-    margin-right: 5px;
-    font-size: 12px;
-    button {
-      display: flex;
-    }
-  }
 `;
 
 const SocialActions = styled.div`
-  align-items: center;
   display: flex;
-  justify-content: flex-start;
-  margin: 0;
-  min-height: 40px;
-  padding: 4px 8px;
+  justify-content: space-around;
+  padding: 8px 16px;
   button {
     display: inline-flex;
     align-items: center;
     padding: 8px;
     color: #0a66c2;
-
-    @media (min-width: 768px) {
-      span {
-        margin-left: 8px;
-      }
+    border: none;
+    background: none;
+    img {
+      margin-right: 8px;
+    }
+    &:hover {
+      color: #004182;
     }
   }
 `;
-
 
 export default Main;
